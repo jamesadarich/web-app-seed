5d32bdae79ec0c382dcde278026d0c4d20844c68
James Richford <jamesrichford@googlemail.com>
2016-12-18 17:22:22 +0000

    add plan for tests

--- test/unit-tests/server/startup.spec.ts
+++ test/unit-tests/server/startup.spec.ts
@@ -1,2 +1,11 @@
 import { Expect, Test, TestFixture } from "alsatian";
 
+// ensure serve static setup correctly
+
+// ensure server uses serve static response
+
+// ensure SPA index served
+
+// ensure server started on port
+
+// ensure console logged with startup info
