8765678e4961ee3d40e050a48fe3c9611df956ad
James Richford <jamesrichford@googlemail.com>
2017-01-29 16:45:29 +0000

    add gzip compression

--- server/startup.ts
+++ server/startup.ts
@@ -1,9 +1,13 @@
+import * as Compression from "compression";
 import * as Express from "express";
 import * as Path from "path";
 import * as ServeStatic from "serve-static";
 
 // create server
 const server: Express.Application = Express();
+
+// setup compression
+server.use(Compression());
 
 // serve up public folder
 server.use(ServeStatic("public", { index: ["index.html"] }));
