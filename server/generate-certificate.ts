be0e48ef86d99e6df40f857750c2a38d69579a25
James Richford <jrichford@neworbit.co.uk>
2018-05-03 17:44:29 +0100

    tiny tidy up

--- server/generate-certificate.ts
+++ server/generate-certificate.ts
@@ -1,8 +1,10 @@
 import { createCertificate, CertificateCreationResult } from "pem";
+import { writeFile } from "fs";
+import { resolve } from "path";
 
 export async function generateCertificate() {
     return new Promise<CertificateCreationResult>((resolve, reject) => {
-        createCertificate({ selfSigned: true ,altNames: [ "localhost" ] }, (error, result) => {
+        createCertificate({ selfSigned: true, altNames: [ "localhost" ] }, (error, result) => {
             if (error) {
                 return reject(error);
             }
